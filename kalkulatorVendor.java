b2ecffde310abb7824ec21964863c74a319310a9
Ryan Daffa Pratama <900595@HQ.bni.co.id>
2024-09-25 10:51:21 +0700

    edit feature

--- kalkulatorVendor.java
+++ kalkulatorVendor.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class LunchCostSplitter {
+public class kalkulatorVendor {
 
     // Method to calculate the total cost per person considering tax and discount
     public static Map<String, Double> calculateCostPerPerson(Map<String, Double> personToFoodCost, double totalTax, double totalDiscount) {
